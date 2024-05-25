7c021037f43366e2e1439ef2cabe8f623690a78e
Saul Gomes <saul.contatodev@gmail.com>
2024-05-25 20:32:49 -0300

    feat: add app module and import auth module

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
