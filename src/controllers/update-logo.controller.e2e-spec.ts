779610dcee1c87f8065259ff4cf26349896cbb49
Saul Gomes <saul.contatodev@gmail.com>
2024-05-27 21:45:33 -0300

    feat(franchise): add insert/update logo controller and tests

---
+++ src/controllers/update-logo.controller.e2e-spec.ts
@@ -0,0 +1,92 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Update logo (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[PUT] /user/:id/logo', async () => {
+    const user = await prisma.users.create({
+      data: {
+        name: 'Saul Gomes',
+        email: 'saul.contatodev@gmail.com',
+        password: await hash('123456', 8),
+        Franchises: {
+          create: {
+            logoImage: 'imagem-anterior',
+          },
+        },
+      },
+      include: {
+        Franchises: true,
+      },
+    })
+
+    const token = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .put(`/user/${user.Franchises[0].id}/logo`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        logo: 'nova-imagem',
+      })
+
+    expect(response.statusCode).toBe(200)
+
+    const updatedUser = await prisma.users.findUnique({
+      where: {
+        id: user.id,
+      },
+      select: {
+        Franchises: true,
+      },
+    })
+
+    expect(updatedUser?.Franchises[0].logoImage).toEqual('nova-imagem')
+  })
+
+  test('Must not be able to update a user with invalid id', async () => {
+    const user = await prisma.users.create({
+      data: {
+        name: 'Saul Gomes',
+        email: 'saul.contato@gmail.com',
+        password: await hash('123456', 8),
+      },
+    })
+
+    const token = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .get(`/user/invalid-id`)
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(404)
+  })
+
+  test('Must not be able to update a user with invalid token', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/user/invalid-id`)
+      .set('Authorization', 'invalid-token')
+
+    expect(response.statusCode).toBe(401)
+  })
+})
---
+++ src/controllers/update-logo.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, NotFoundException, Param, Put } from '@nestjs/common'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const updateUserBodySchema = z.object({
+  logo: z.string().optional(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(updateUserBodySchema)
+
+type UpdateUserBodySchema = z.infer<typeof updateUserBodySchema>
+
+@Controller('user/:id/logo')
+export class UpdateLogo {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Put()
+  async handle(
+    @Body(bodyValidationPipe) body: UpdateUserBodySchema,
+    @Param('id') id: string,
+  ) {
+    const { logo } = body
+
+    const franchise = await this.prisma.franchises.findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+
+    if (!franchise) throw new NotFoundException('Resource not found.')
+
+    await this.prisma.franchises.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        logoImage: logo,
+      },
+    })
+  }
+}
