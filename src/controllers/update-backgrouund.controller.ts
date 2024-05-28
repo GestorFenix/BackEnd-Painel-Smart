d390fffaaeeb5e8e5ae12118cfd03f708a3ec306
Saul Gomes <saul.contatodev@gmail.com>
2024-05-27 21:45:08 -0300

    feat(franchise): add insert/update background image controller and tests

---
+++ src/controllers/update-backgrouund.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, NotFoundException, Param, Put } from '@nestjs/common'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const updateUserBodySchema = z.object({
+  background: z.string().optional(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(updateUserBodySchema)
+
+type UpdateUserBodySchema = z.infer<typeof updateUserBodySchema>
+
+@Controller('user/:id/background')
+export class UpdateBackground {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Put()
+  async handle(
+    @Body(bodyValidationPipe) body: UpdateUserBodySchema,
+    @Param('id') id: string,
+  ) {
+    const { background } = body
+
+    const franchise = await this.prisma.franchises.findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+
+    if (!franchise) throw new NotFoundException('Resource not found.')
+
+    await this.prisma.franchises.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        backgroundImage: background,
+      },
+    })
+  }
+}
