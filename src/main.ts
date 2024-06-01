47c71d28a50ab1976bc4f73283ec1a6849400826
Saul Gomes <saul.contatodev@gmail.com>
2024-06-01 20:29:09 -0300

    change imports position

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
-import { Env } from './env'
+import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import type { Env } from './env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['warn', 'error'],
-  })
+	const app = await NestFactory.create(AppModule, {
+		logger: ['warn', 'error'],
+	})
 
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+	const configService = app.get<ConfigService<Env, true>>(ConfigService)
+	const port = configService.get('PORT', { infer: true })
 
-  const config = new DocumentBuilder()
-    .setTitle('Infinity')
-    .setVersion('1.0')
-    .build()
+	const config = new DocumentBuilder()
+		.setTitle('Infinity')
+		.setVersion('1.0')
+		.build()
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('docs', app, document)
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document)
 
-  await app.listen(port)
+	await app.listen(port)
 }
 bootstrap()
