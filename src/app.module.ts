4bdbd7c2eb3f3e84c72cf79c44cee8cf50b82280
Saul Gomes <saul.contatodev@gmail.com>
2024-06-01 20:28:57 -0300

    style(app): change imports and quotes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,41 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateUserController } from './controllers/create-user.controller'
 import { DeleteUserController } from './controllers/delete-user-by-id.controller'
 import { GetUserByIdController } from './controllers/get-user-by-id.controller'
+import { GetUsersOnlineController } from './controllers/get-users-online.controller'
+import { GetUsersController } from './controllers/get-users.controller'
+import { UpdateBackground } from './controllers/update-backgrouund.controller'
+import { UpdateClientsLimit } from './controllers/update-clients-limit.controller'
+import { UpdateLogo } from './controllers/update-logo.controller'
 import { UpdatePasswordController } from './controllers/update-user-password.controller'
 import { UpdateUserController } from './controllers/update-user.controller'
-import { GetUsersController } from './controllers/get-users.controller'
-import { UpdateLogo } from './controllers/update-logo.controller'
-import { UpdateBackground } from './controllers/update-backgrouund.controller'
-import { GetUsersOnlineController } from './controllers/get-users-online.controller'
-import { UpdateClientsLimit } from './controllers/update-clients-limit.controller'
+import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [
-    AuthModule,
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-  ],
-  providers: [PrismaService],
-  controllers: [
-    AuthenticateController,
-    CreateUserController,
-    DeleteUserController,
-    GetUserByIdController,
-    GetUsersController,
-    UpdatePasswordController,
-    UpdateUserController,
-    UpdateLogo,
-    UpdateBackground,
-    GetUsersOnlineController,
-    UpdateClientsLimit,
-  ],
+	imports: [
+		AuthModule,
+		ConfigModule.forRoot({
+			validate: (env) => envSchema.parse(env),
+			isGlobal: true,
+		}),
+	],
+	providers: [PrismaService],
+	controllers: [
+		AuthenticateController,
+		CreateUserController,
+		DeleteUserController,
+		GetUserByIdController,
+		GetUsersController,
+		UpdatePasswordController,
+		UpdateUserController,
+		UpdateLogo,
+		UpdateBackground,
+		GetUsersOnlineController,
+		UpdateClientsLimit,
+	],
 })
 export class AppModule {}
