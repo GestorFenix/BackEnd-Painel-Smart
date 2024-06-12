01be1beeea8499abd8e55e1c9e11795e193ae4fa
Saul Gomes <saul.contatodev@gmail.com>
2024-06-11 22:57:07 -0300

    fix(update-client-limit): remove optional in zod object

--- src/controllers/update-clients-limit.controller.ts
+++ src/controllers/update-clients-limit.controller.ts
@@ -5,7 +5,7 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { z } from 'zod'
 
 const updateUserBodySchema = z.object({
-  clientLimit: z.string().optional(),
+  clientLimit: z.number(),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(updateUserBodySchema)
@@ -18,12 +18,16 @@ export class UpdateClientsLimit {
   constructor(private readonly prisma: PrismaService) { }
 
   @Put()
-  @ApiOperation({ summary: 'Rota para atualizar o limite de clientes da franquia.' })
+  @ApiOperation({
+    summary: 'Rota para atualizar o limite de clientes da franquia.',
+  })
   async handle(
     @Body(bodyValidationPipe) body: UpdateUserBodySchema,
     @Param('id') id: string,
   ) {
     const { clientLimit } = body
+
+    console.log(clientLimit)
 
     const franchise = await this.prisma.franchises.findUnique({
       where: {
