3d1f755b46eb96413c8546f2c80b806ce626b8b0
Saul Gomes <saul.contatodev@gmail.com>
2024-06-11 22:56:29 -0300

    fix(get-user): return id and client limit in each user

--- src/controllers/get-users.controller.ts
+++ src/controllers/get-users.controller.ts
@@ -5,19 +5,22 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger'
 @Controller('users')
 @ApiTags('Franquias')
 export class GetUsersController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   @Get()
   @ApiOperation({ summary: 'Retorna todos as franquias e suas informções.' })
   async handle() {
     return await this.prisma.users.findMany({
       select: {
+        id: true,
         email: true,
         name: true,
         lastLogin: true,
         Franchises: {
           select: {
+            id: true,
             dns: true,
+            clientLimit: true,
           },
         },
       },
